754075fc4ccdb6121c1609c51db425e7c93f03f3
Maitan <victormaitan01@gmail.com>
2021-01-05 03:26:44 -0400

    Added register product

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,7 +11,7 @@
             label="Adicionar"
             class="bg-warning text-red-8 q-mr-md"
             icon-right="add"
-            @click="$router.push({ name: 'Adicionar' })"
+            @click="dialogAdd = true"
           />
         </div>
         <div>
@@ -60,6 +60,46 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-dialog v-model="dialogAdd" persistent>
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-card-section>
+          <div class="text-h6">Adicionar</div>
+        </q-card-section>
+
+        <q-card-section class="q-gutter-sm">
+          <div class="q-gutter-sm">
+            <q-radio
+              color="red-8"
+              v-model="typeAdd"
+              val="burgers"
+              label="Burgers"
+            />
+            <q-radio
+              color="red-8"
+              v-model="typeAdd"
+              val="pizzas"
+              label="Pizzas"
+            />
+          </div>
+          <q-input filled v-model="product.title" label="Nome" />
+          <q-input filled v-model="product.description" label="Descrição" />
+          <q-input filled v-model="product.picture" label="Link da imagem" />
+          <q-input filled type="number" v-model="product.price" label="Preço" />
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn class="text-red-8" flat label="Cancelar" v-close-popup />
+          <q-btn
+            class="text-green-6"
+            flat
+            label="Adicionar"
+            @click="add()"
+            v-close-popup
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-layout>
 </template>
 
@@ -70,7 +110,19 @@ export default {
     return {
       classProducts: 'bg-grey-3 text-black',
       classBurgers: 'bg-red-8 text-white',
-      classPizzas: 'bg-red-8 text-white'
+      classPizzas: 'bg-red-8 text-white',
+
+      dialogAdd: false,
+      product: {
+        id: '',
+        picture: '',
+        title: '',
+        stars: 0,
+        price: 0,
+        description: ''
+      },
+
+      typeAdd: ''
     }
   },
   methods: {
@@ -98,6 +150,35 @@ export default {
           break
         }
       }
+    },
+
+    add () {
+      const arrayBurgers = JSON.parse(window.localStorage.getItem('burgers'))
+      const arrayPizzas = JSON.parse(window.localStorage.getItem('pizzas'))
+
+      if (this.typeAdd === 'burgers') {
+        this.product.id = arrayBurgers.length
+        arrayBurgers.push(this.product)
+        window.localStorage.setItem('burgers', JSON.stringify(arrayBurgers))
+        this.concatProducts()
+      } else if (this.typeAdd === 'pizzas') {
+        this.product.id = arrayPizzas.length
+        arrayPizzas.push(this.product)
+        window.localStorage.setItem('pizzas', JSON.stringify(arrayPizzas))
+        this.concatProducts()
+      }
+    },
+
+    concatProducts () {
+      function randOrd () {
+        return Math.round(Math.random()) - 0.5
+      }
+      const array = JSON.parse(window.localStorage.getItem('burgers')).concat(
+        JSON.parse(window.localStorage.getItem('pizzas'))
+      )
+      array.sort(randOrd)
+      this.products = array
+      window.localStorage.setItem('products', JSON.stringify(this.products))
     }
   }
 }
